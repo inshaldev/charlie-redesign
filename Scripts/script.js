55a18428961e6d3dc61b04298c66bfca52940192
inshalworks <88522625+inshalworks@users.noreply.github.com>
2021-12-06 19:05:38 +0500

    COMPLETED :)))

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,26 +1,22 @@
 const navbarSection = document.querySelector('#navbar-section');
 const closeBtn = document.querySelector('#toggle-nav');
+const allNavLinks = document.querySelectorAll('.navbar-list-link');
 let navState = 0;
-const toggleNav = (x) => {
-  x.classList.toggle('change');
+const toggleNav = () => {
+  closeBtn.classList.toggle('change');
   navbarSection.classList.toggle('opened');
-  navbarSection.classList.toggle('none', false);
+  navbarSection.classList.remove('none');
 };
+allNavLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    navbarSection.classList.remove('opened');
+    closeBtn.classList.remove('change');
+  });
+});
 // NAVBAR DONE
-// const toggleActiveLink = (link) => {
-//   console.log(link.attributes[0].value);
-// };
-
-// const links = document.querySelectorAll('.navbar-list-link');
-// links.forEach((link) =>
-//   link.addEventListener('click', () => {
-//     toggleActiveLink(link);
-//   })
-// );
 window.addEventListener('scroll', () => {
   let currentSection = '';
   const allSections = document.querySelectorAll('.section');
-  const allNavLinks = document.querySelectorAll('.navbar-list-link');
   allSections.forEach((section) => {
     const sectionOffset = section.offsetTop;
     const sectionHeight = section.clientHeight;
