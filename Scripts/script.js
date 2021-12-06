f1ceead724a676886a5027149b4dfb5ec6595366
inshalworks <88522625+inshalworks@users.noreply.github.com>
2021-12-06 17:59:17 +0500

    complete :) almost except social icons, footer & animations :)

--- Scripts/script.js
+++ Scripts/script.js
@@ -6,3 +6,33 @@ const toggleNav = (x) => {
   navbarSection.classList.toggle('opened');
   navbarSection.classList.toggle('none', false);
 };
+// NAVBAR DONE
+// const toggleActiveLink = (link) => {
+//   console.log(link.attributes[0].value);
+// };
+
+// const links = document.querySelectorAll('.navbar-list-link');
+// links.forEach((link) =>
+//   link.addEventListener('click', () => {
+//     toggleActiveLink(link);
+//   })
+// );
+window.addEventListener('scroll', () => {
+  let currentSection = '';
+  const allSections = document.querySelectorAll('.section');
+  const allNavLinks = document.querySelectorAll('.navbar-list-link');
+  allSections.forEach((section) => {
+    const sectionOffset = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (scrollY >= sectionOffset - sectionHeight / 3) {
+      currentSection = section.getAttribute('id');
+    }
+  });
+
+  allNavLinks.forEach((li) => {
+    li.classList.remove('navbar-link-active');
+    if (li.classList.contains(currentSection)) {
+      li.classList.add('navbar-link-active');
+    }
+  });
+});
